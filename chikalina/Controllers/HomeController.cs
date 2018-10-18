7347f8ceb24c54845c8c638967e4292568685ec8
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-10-17 23:55:54 -0300

    novos

---
+++ chikalina/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using chikalina.Models;
+using Chikalina.Models;
+using Facebook;
+using Microsoft.Extensions.Options;
+
+namespace chikalina.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<Fotos> Fotos { get; set; }
+        private readonly IOptions<Keys> _config;
+
+        public HomeController(IOptions<Keys> config)
+        {
+            _config = config;
+        }
+
+
+        public ActionResult Index()
+        {
+            Fotos = new List<Fotos>();
+            var appid = _config.Value.FacebookAppId;
+            var appsecret = _config.Value.FacebookAppSecret;
+            string appAccessToken = string.Concat(appid, "|", appsecret);
+            var fb = new FacebookClient(appAccessToken);
+            dynamic result = fb.Get("1219310658089410?fields=albums.fields(photos.fields(source))");
+            dynamic fotos = result["albums"]["data"][0]["photos"][0];
+
+            foreach (var item in fotos)
+            {
+                if (Fotos.Count < 24)
+                {
+                    Fotos.Add(new Fotos { Url = item.source });
+                }
+            }
+
+            ViewBag.FacebookAppId = appid;
+            return View(Fotos);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ chikalina/Models/Keys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chikalina.Models
+{
+    public class Keys
+    {
+        public string FacebookAppId { get; set; }
+        public string FacebookAppSecret { get; set; }
+    }
+}
